91dad6b7054b724c6bcf3df8c7234d4e1a6fff0a
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-07-16 15:14:33 -0700

    start writing crawler framework

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # wiktionary_crawler
 Crawl Wiktionary Courteously yet Correctly and Persistently
 
+```rust
+cargo run
+```
+
 ## Contribution
 Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in wiktionary_crawler by you,
 shall be dual licensed under the MIT and Apache 2.0 license without any additional terms or conditions.
--- src/bin_crawl.rs
+++ src/bin_crawl.rs
@@ -1,2 +1,16 @@
-fn main() {
+use error_chain::error_chain;
+error_chain! {
+     foreign_links {
+         Io(std::io::Error);
+         HttpRequest(reqwest::Error);
+     }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let seed = "https://en.wiktionary.org/wiki/seed";
+    let response = reqwest::get(seed).await?;
+    let _content =  response.text().await?;
+
+    Ok(())
 }
