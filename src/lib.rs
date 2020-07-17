53619fd5db5410b0987b8e1aa42100fb42c0f1ec
Andrew Johnson <andrewjohnson@Andrews-MacBook-Pro.local>
2020-07-16 19:53:32 -0700

    serde_with for wiktionary entry data

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
+use serde_with::skip_serializing_none;
+use serde::{Serialize,Deserialize};
 
+#[skip_serializing_none]
+#[derive(Deserialize, Serialize)]
 pub struct WiktionaryEntry {
    word: String,
-   language: Option<String>
-   ipa_pronunciation: Vec<String>
+   language: Option<String>,
+   ipa_pronunciation: Option<Vec<String>>,
 }
